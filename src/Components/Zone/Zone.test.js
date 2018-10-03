3ba40fba4a7f81c43867223304641a8666c5be7f
Tory Dannenberg <rfdberg@gmail.com>
2018-10-02 20:01:28 -0600

    Install indentity obj proxy and create test files for all Componenents

---
+++ src/Components/Zone/Zone.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Zone from './Zone.js';
+
+describe('Zone tests', () => {
+  it('should return true', () => {
+    expect(true).toEqual(true);
+  });
+});
---
+++ src/Components/ZoneContainer/ZoneContainer.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ZoneContainer from './ZoneContainer.js';
+
+describe('ZoneContainer tests', () => {
+  it('should return true', () => {
+    expect(true).toEqual(true);
+  });
+});
