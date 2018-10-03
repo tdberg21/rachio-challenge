e786721c4b92b7b11fde2bdc907f714abd96874f
Tory Dannenberg <rfdberg@gmail.com>
2018-10-03 16:10:55 -0600

    Add tests for DeviceContainer

--- src/Components/DeviceContainer/DeviceContainer.test.js
+++ src/Components/DeviceContainer/DeviceContainer.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import DeviceContainer from './DeviceContainer.js';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('DeviceContainer tests', () => {
-  it('should return true', () => {
-    expect(true).toEqual(true);
+
+  it('matches the snapshot without devices', () => {
+    const wrapper = shallow(<DeviceContainer />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('matches the snapshot with devices', () => {
+    let mockDevices = [{ id: 1 }, { id: 2 }];
+    const wrapper = shallow(<DeviceContainer devices={mockDevices} />);
+    expect(wrapper).toMatchSnapshot();
   });
 });
